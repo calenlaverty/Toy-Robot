5638128ac7c279c0d2f01c8fc7b5b740ba9e48f9
Calen L <140864859+calenlaverty@users.noreply.github.com>
2025-04-29 20:51:54 +1000

    Vue working with a basic component injected

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      message: "Hello Vue with TypeScript!",
+    };
+  },
+});
+</script>
