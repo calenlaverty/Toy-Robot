6f9e247d21e8d8056ed71fa828c17c8e5f37d973
Calen L <140864859+calenlaverty@users.noreply.github.com>
2025-06-03 18:21:43 +1000

    Adding Auth0 login

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from "vue";
+import { createAuth0 } from "@auth0/auth0-vue";
 import App from "./components/App.vue";
 
 const app = createApp(App);
+
+app.use(
+  createAuth0({
+    domain: "dev-sh27mvp4kn6uu1kw.us.auth0.com",
+    clientId: "2gR2jJh1yLzGEYFY94MZSdEOzla8DcjU",
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+    },
+  })
+);
+
 app.mount("#app");
